65ce46ae987a78188eadc2ab97426729f6e5cc2f
jpromanonet <programming@jpromano.net>
2020-02-29 11:27:31 -0300

    Finished Week07_04

---
+++ week7-week7_04.ThingSuitcaseAndContainer/src/Thing.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Thing {
+    private String name;
+    private int weight;
+
+    public Thing(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+
+    public void setWeight(int weight) {
+        this.weight = weight;
+    }
+
+    @Override
+    public String toString() {
+        return this.name + " (" + this.weight + " kg)";
+    }
+
+
+
+}
