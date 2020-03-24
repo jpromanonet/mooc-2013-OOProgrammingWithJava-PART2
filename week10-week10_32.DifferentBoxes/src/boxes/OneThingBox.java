9e584eac7f4f30581ccbef7c43e33329a130be1f
jpromanonet <programming@jpromano.net>
2020-03-24 14:13:26 -0300

    Week 10 - Ex.06

---
+++ week10-week10_32.DifferentBoxes/src/boxes/OneThingBox.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package boxes;
+
+public class OneThingBox extends Box {
+
+    private Thing thing;
+
+    @Override
+    public void add(Thing thing) {
+        if (this.thing == null) {
+            this.thing = thing;
+        }
+    }
+
+    @Override
+    public boolean isInTheBox(Thing thing) {
+        return this.thing != null && this.thing.equals(thing);
+    }
+}
