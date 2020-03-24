b54f8327532d26aac0db7536a0ed3b598a8bed42
jpromanonet <programming@jpromano.net>
2020-03-24 16:48:55 -0300

    Course finished

---
+++ week12-week12_50.CourseFeedback/src/Main.java
@@ -0,0 +1,16 @@
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("" + 
+                "Course is over!\n" + 
+                "After you have submitted this exercise, remember to give us " + 
+                "some feedback about this course\n");
+    
+        System.out.println("The course has been great. It would truly benefit from a third part, discussing topics like " +
+                "threading and concurrency, dependency management/build tools(Maven, Gradle etc) and unit testing." +
+                "I tried learning from the tests in the course but most variable names are in Finnish so that didn't work out." +
+                "There are minor issues with the translation in many parts of the course but nothing problematic." +
+                "Overall this has to be the best course I've done so far.");
+    }
+}
